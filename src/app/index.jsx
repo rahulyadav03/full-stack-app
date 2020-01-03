4a7c8b5384f3829b9c56383aa77ba220ab8d7ab7
Rahul Yadav <rahul.yadav@nagarro.com>
2020-01-03 13:46:35 +0530

    working on the app

---
+++ src/app/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Store } from './store'
+import { Main } from './components/Main';
+
+ReactDOM.render(
+    <Main />, 
+    document.getElementById("app")
+);
